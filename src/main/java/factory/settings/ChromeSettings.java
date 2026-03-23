f199999e953086cd4bacfef1590be2b758d360fc
CodeWeiName <128158298+CodeWeiName@users.noreply.github.com>
2026-03-23 19:44:29 +0300

    return chrome settings

--- src/main/java/factory/settings/ChromeSettings.java
+++ src/main/java/factory/settings/ChromeSettings.java
@@ -5,13 +5,20 @@ import org.openqa.selenium.remote.AbstractDriverOptions;
 
 public class ChromeSettings implements ISettings {
 
+//  @Override
+//  public AbstractDriverOptions setting() {
+//    ChromeOptions options = new ChromeOptions();
+//    options.addArguments("--headless=new");
+//    options.addArguments("--no-sandbox");
+//    options.addArguments("--disable-dev-shm-usage");
+//    options.addArguments("--window-size=1920,1080");
+//    return options;
+//  }
+
   @Override
   public AbstractDriverOptions setting() {
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--headless=new");
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--window-size=1920,1080");
-    return options;
+    ChromeOptions chromeOptions = new ChromeOptions();
+    chromeOptions.addArguments("--start-maximized");
+    return chromeOptions;
   }
 }
